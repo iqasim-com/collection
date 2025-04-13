685340b8d7e50becc712e18d8529fbb4b99518e4
Muhammad Qasim <qasim.tanxe@gmail.com>
2025-04-13 14:16:34 +0500

    [Calculator] - Init commit

---
+++ calculator/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Calculator</h1>
+      </div>
+      <div className="wrapper">
+        <input type="text"/>
+      </div>
+    </>
+  )
+}
+
+export default App
