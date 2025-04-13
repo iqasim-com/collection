4948bad493ca67d22d79bc2282c4582e75f21370
Muhammad Qasim <qasim.tanxe@gmail.com>
2025-04-13 14:23:11 +0500

    [Calculator] - Added tailwind and updated configurations

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Calculator</h1>
+        <h1 className="text-3xl">Calculator</h1>
       </div>
       <div className="wrapper">
         <input type="text"/>
