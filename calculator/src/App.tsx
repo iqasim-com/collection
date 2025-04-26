98a7b5833be6f0f1f6435e9627c8eacb319fd2de
Muhammad Qasim <qasim.tanxe@gmail.com>
2025-04-26 23:04:06 +0500

    [Calculator] - Updated styles and text

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -19,13 +19,13 @@ function App() {
   return (
     <>
       <div className="flex justify-center align-center h-full flex-col items-center">
-        <div className="inline w-1/2 border border-gray-300 rounded-md p-4">
+        <div className="inline border border-gray-300 rounded-md p-4">
           <div className="mb-10">
             <Input additionalClasses="text-4xl p-4 w-full" placeholder="0" value={input} />
           </div>
           <div>
             <div className="grid grid-cols-3 gap-2">
-              <Button variant="danger" click={clear}>Clear</Button>
+              <Button variant="danger" click={clear}>AC</Button>
               {buttons.map((button, index) => (
                 <Button
                   key={Date.now() + index}
